bb01537c0f12ddcf77f5d167758400d3b92d42e2
AYeshsidd <hafizayeshsidd@gmail.com>
2026-07-21 00:12:48 +0500

    Role based auth & product details managed

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -13,12 +13,13 @@ Purpose:
 - Verify it (signature + expiry) using decode_token().
 - Look up the user in the database.
 - Give the current logged-in user to any route that needs it.
+- check permisions for current logged-in user like he can delete/update/remove.
+
 """
 
 # This tells FastAPI: "expect a simple Bearer token in the
 # Authorization header — just a token, no username/password form."
 bearer_scheme = HTTPBearer()
-
 
 def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
@@ -51,3 +52,14 @@ def get_current_user(
         raise credentials_exception
 
     return user
+
+# ROLE BASED
+def require_role(*allowed_roles: str):
+    def role_checker(current_user: Users = Depends(get_current_user)) -> Users:
+        if current_user.role not in allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You don't have permission to perform this action"
+            )
+        return current_user
+    return role_checker
